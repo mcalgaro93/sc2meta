757009fe1e4b082e109137a9c21833678e4b8125
mcalgaro93 <mcalgaro93@gmail.com>
2020-03-28 15:07:15 +0100

    light data version

--- type_I_error_control/mock_16S_comparison_script.R
+++ type_I_error_control/mock_16S_comparison_script.R
@@ -1,6 +1,7 @@
 library(phyloseq)
 source("../eval_functions.R")
-ps_list <- readRDS(file = "../data/Stool_16S_WMS_mock.RDS")
+load(file = "../data/Stool_16S_WMS.RData")
+mock_df <- read.table(file = "../data/Stool_16S_WMS_mock_df.tsv",sep = "\t")
 
 args <- commandArgs(trailingOnly = TRUE)
 str(args)
@@ -8,7 +9,8 @@ cat(args, sep = "\n")
 
 task_id <- as.numeric(args[1])
 
-ps <- ps_list$Stool_16S[[task_id]]
+ps <- Stool_16S_WMS$Stool_16S
+ps@sam_data$grp <- unlist(mock_df[task_id,])
 Stool_16S_mockDA <- oneSimRunGSOwn(physeq = ps,epsilon = 1e10)
 
 saveRDS(object = Stool_16S_mockDA,file = paste0("../mockDA/Stool_16S_mockDA_",task_id,".RDS"))
--- type_I_error_control/mock_WMS_comparison_script.R
+++ type_I_error_control/mock_WMS_comparison_script.R
@@ -1,6 +1,7 @@
 library(phyloseq)
 source("../eval_functions.R")
-ps_list <- readRDS(file = "../data/Stool_16S_WMS_mock.RDS")
+load(file = "../data/Stool_16S_WMS.RData")
+mock_df <- read.table(file = "../data/Stool_16S_WMS_mock_df.tsv",sep = "\t")
 
 args <- commandArgs(trailingOnly = TRUE)
 str(args)
@@ -8,7 +9,8 @@ cat(args, sep = "\n")
 
 task_id <- as.numeric(args[1])
 
-ps <- ps_list$Stool_WMS[[task_id]]
+ps <- Stool_16S_WMS$Stool_WMS
+ps@sam_data$grp <- unlist(mock_df[task_id,])
 Stool_WMS_mockDA <- oneSimRunGSOwn(physeq = ps,epsilon = 1e10)
 
 saveRDS(object = Stool_WMS_mockDA,file = paste0("../mockDA/Stool_WMS_mockDA_",task_id,".RDS"))
