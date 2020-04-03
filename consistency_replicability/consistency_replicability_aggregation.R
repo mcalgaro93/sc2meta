406f0831c67d488238a2e90e3b47e12fb50fce2b
mcalgaro93 <mcalgaro93@gmail.com>
2020-04-03 23:39:53 +0200

    songbird results joined to all the other methods

--- consistency_replicability/consistency_replicability_aggregation.R
+++ consistency_replicability/consistency_replicability_aggregation.R
@@ -23,7 +23,7 @@ subsets_replicability_DA_16S <- list(subgingival_supragingival = subgingival_sup
                                      gingiva_mucosa = gingiva_mucosa,
                                      tonguedorsum_stool = tonguedorsum_stool)
 
-saveRDS(subsets_replicability_DA_16S,file = "../data/16Ssubsets_replicability_DA.RData")
+saveRDS(subsets_replicability_DA_16S,file = "../data/16Ssubsets_replicability_DA.RDS")
 
 # WMS
 CRC_control <- list()
@@ -48,4 +48,107 @@ WMSsubsets_replicability_DA <- list(CRC_control = CRC_control,
                                     schizophrenia_control = schizophrenia_control,
                                     tonguedorsum_stool = tonguedorsum_stool)
 
-saveRDS(WMSsubsets_replicability_DA,file = "../data/WMSsubsets_replicability_DA.RData")
+saveRDS(WMSsubsets_replicability_DA,file = "../data/WMSsubsets_replicability_DA.RDS")
+
+################ songbird ##############################
+
+songbird_data_collector <- function(filename,sequence = 1:100){
+  comparisons <- list()
+  for(i in sequence){
+    subsets <- list()
+    for(j in 1:2){
+      subset = read.table(file = paste0(filename,i,"_Subset",j,"/differentials.tsv"),
+                          header = TRUE,sep = "\t",row.names = 1)
+      subsets[[j]] <- subset
+    }
+    names(subsets) <- c("Subset1","Subset2")
+    comparisons[[i]] <- subsets
+  }
+  names(comparisons) <- paste0("Comparison",1:100)
+  return(comparisons)
+}
+
+# 16S
+filename = "./songbird/16S_subgingival_supragingival_Comparison"
+subgingival_supragingival <- songbird_data_collector(filename)
+
+filename = "./songbird/16S_gingiva_mucosa_Comparison"
+gingiva_mucosa <- songbird_data_collector(filename)
+
+filename = "./songbird/16S_tonguedorsum_stool_Comparison"
+# Since some corncob method failed in some sims, this is the matched sequence to obtain 100 sims
+sequence = c(1:5,101,7:10,120,122,102,14:16,103,18:25,104,27:31,124,33:39,105,
+             41:50,106,52:53,107,55:56,108,58:59,109,61:64,111,66:67,112,69,113,
+             71,115,116,74:80,119,82)
+tonguedorsum_stool <- songbird_data_collector(filename)
+
+subsets_replicability_DA_16S_songbird <- list(subgingival_supragingival = subgingival_supragingival,
+                                              gingiva_mucosa = gingiva_mucosa,
+                                              tonguedorsum_stool = tonguedorsum_stool)
+
+saveRDS(subsets_replicability_DA_16S_songbird,file = "../data/16Ssubsets_replicability_DA_songbird.RDS")
+
+# WMS
+filename = "./songbird/WMS_CRC_control_Comparison"
+CRC_control <- songbird_data_collector(filename)
+
+filename = "./songbird/WMS_schizophrenia_control_Comparison"
+schizophrenia_control <- songbird_data_collector(filename)
+
+filename = "./songbird/WMS_tonguedorsum_stool_Comparison"
+tonguedorsum_stool <- songbird_data_collector(filename)
+
+WMSsubsets_replicability_DA_songbird <- list(CRC_control = CRC_control,
+                                             schizophrenia_control = schizophrenia_control,
+                                             tonguedorsum_stool = tonguedorsum_stool)
+
+saveRDS(WMSsubsets_replicability_DA_songbird,file = "../data/WMSsubsets_replicability_DA_songbird.RDS")
+
+#############################################
+#                                           #
+## put together songbird and other methods ##
+#                                           #
+#############################################
+
+join_all_methods <- function(method_list,songbird_list){
+  
+  for(dataset in names(method_list)){
+    # print(paste0("dataset: ",dataset))
+    data <- method_list[[dataset]]
+    data_songbird <- songbird_list[[dataset]]
+    comparisons <- list()
+    for(i in 1:100){
+      # print(paste0("comparison: ",i))
+      comparison <- data[[i]]
+      comparison_songbird <- data_songbird[[i]]
+      subsets <- list()
+      for(j in 1:2){
+        # print(paste0("subset: ",j))
+        subset <- comparison[[j]]
+        subset_songbird <- comparison_songbird[[j]]
+        subset$songbird <- subset_songbird
+        subsets[[j]] <- subset
+        # print(paste(names(subset)))
+      }
+      names(subsets) <- c("Subset1","Subset2")
+      comparisons[[i]] <- subsets
+    }
+    names(comparisons) <- paste0("Comparison",1:100)
+    method_list[[dataset]] <- comparisons
+  }
+  return(method_list)
+}
+
+WMSsubsets_replicability_DA <- readRDS(file = "../data/WMSsubsets_replicability_DA.RDS")
+subsets_replicability_DA_16S <- readRDS(file = "../data/16Ssubsets_replicability_DA.RDS")
+WMSsubsets_replicability_DA_songbird <- readRDS(file = "../data/WMSsubsets_replicability_DA_songbird.RDS")
+subsets_replicability_DA_16S_songbird <- readRDS(file = "../data/16Ssubsets_replicability_DA_songbird.RDS")
+
+WMSsubsets_replicability_DA_total <- join_all_methods(method_list = WMSsubsets_replicability_DA,
+                                                      songbird_list = WMSsubsets_replicability_DA_songbird)
+subsets_replicability_DA_total_16S <- join_all_methods(method_list = subsets_replicability_DA_16S,
+                                                       songbird_list = subsets_replicability_DA_16S_songbird)
+
+saveRDS(WMSsubsets_replicability_DA_total,file = "../data/WMSsubsets_replicability_DA_total.RDS")
+saveRDS(subsets_replicability_DA_total_16S,file = "../data/16Ssubsets_replicability_DA_total.RDS")
+
